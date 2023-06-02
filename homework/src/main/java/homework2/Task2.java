e4fb566f58b2ff3358b9f732dc4ccc4627fbfaf3
Denys Khvostenko <deni4ka88@gmail.com>
2023-06-02 11:47:04 -0500

    change

--- homework/src/main/java/homework2/Task2.java
+++ homework/src/main/java/homework2/Task2.java
@@ -6,7 +6,7 @@ public class Task2 {
         int parentsMoney = 5000;
         int euro = 28;
         int allVasayMoney = vasyaMoney + parentsMoney;
-        double result = allVasayMoney/euro;
+        double result = allVasayMoney / euro;
         System.out.println(result);
     }
 }
