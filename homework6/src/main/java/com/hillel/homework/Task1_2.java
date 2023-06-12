f74a9eede9cca43a450e6a50dd3f8680e4d92304
Denys Khvostenko <deni4ka88@gmail.com>
2023-06-12 10:54:42 -0500

    added changes in tasks

---
+++ homework6/src/main/java/com/hillel/homework/Task1_2.java
@@ -0,0 +1,13 @@
+package com.hillel.homework;
+
+public class Task1_2 {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        int j = 0;
+        for (int i = 0; i < a.length; i++) {
+            j = j + 2;
+            a[i] += j;
+            System.out.print(a[i] + " ");
+        }
+    }
+}
