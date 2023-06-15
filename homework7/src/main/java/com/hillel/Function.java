4be356f5e45c49234650e8168d61fc5df1553185
Denys Khvostenko <deni4ka88@gmail.com>
2023-06-15 12:11:47 -0500

    change "Function"

--- homework7/src/main/java/com/hillel/Function.java
+++ homework7/src/main/java/com/hillel/Function.java
@@ -35,7 +35,7 @@ public class Function {
                 result = num1 * num2;
                 break;
             default:
-                throw new IllegalArgumentException("Невідома операція: " + operation);
+                throw new IllegalArgumentException("unknown operation: " + operation);
         }
         return result;
     }
