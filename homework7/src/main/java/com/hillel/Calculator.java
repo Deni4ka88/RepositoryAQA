6a71358dc76bf94404a7d749b68b12e476f28f3f
Denys Khvostenko <deni4ka88@gmail.com>
2023-06-16 14:32:29 -0500

    change scanner

--- homework7/src/main/java/com/hillel/Calculator.java
+++ homework7/src/main/java/com/hillel/Calculator.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        int firstNum = Function.getInt();
-        int secondNum = Function.getInt();
-        char operation = Function.getOperation();
-        int result = Function.calc(firstNum,secondNum,operation);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num1 = Function.getInt(scanner);
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num2 = Function.getInt(scanner1);
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter method");
+        char operation = Function.getOperation(scanner2);
+
+        int result = Function.calc(num1,num2,operation);
         System.out.println(result);
     }
 }
--- homework7/src/main/java/com/hillel/Function.java
+++ homework7/src/main/java/com/hillel/Function.java
@@ -3,20 +3,12 @@ package com.hillel;
 import java.util.Scanner;
 
 public class Function {
-    public static int getInt() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number");
-        int number = scanner.nextInt();
-        scanner.nextLine();
-        return number;
+    public static int getInt(Scanner scanner) {
+         return  scanner.nextInt();
     }
 
-    public static char getOperation() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter math method");
-        char operation = scanner.nextLine().charAt(0);
-        scanner.close();
-        return operation;
+    public static char getOperation(Scanner scanner) {
+        return scanner.nextLine().charAt(0);
     }
 
     public static int calc(int num1, int num2, char operation) {
